bb230aa2793fe2ebdb272b779646305640ba6b42
farhanshidik653-cmyk <farhanshidik653@gmail.com>
2026-05-26 16:36:09 +0700

    index.cjs

--- index.cjs
+++ index.cjs
@@ -1,16 +1,49 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./chat.db');
+
+db.run(`
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user TEXT,
+  text TEXT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+`);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+
+  console.log('User connected');
+
   socket.on('chat message', (msg) => {
+
+    db.run(
+      'INSERT INTO messages (user, text) VALUES (?, ?)',
+      [msg.user, msg.text],
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
+
     io.emit('chat message', msg);
   });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
