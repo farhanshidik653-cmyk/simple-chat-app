5141dfea59e80d07897d80f0a22549fa384a3aad
farhanshidik653-cmyk <farhanshidik653@gmail.com>
2026-05-26 15:55:33 +0700

    index.cjs

--- index.cjs
+++ index.cjs
@@ -13,6 +13,8 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
